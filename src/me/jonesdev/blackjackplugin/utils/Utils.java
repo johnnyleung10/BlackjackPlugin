8040d950f9c551a6e5e3858de632867611ca99af
johnnyleung10 <johnnyleung2001@gmail.com>
2019-08-14 14:02:26 -0700

    Project setup

---
+++ src/me/jonesdev/blackjackplugin/utils/Utils.java
@@ -0,0 +1,9 @@
+package me.jonesdev.blackjackplugin.utils;
+
+import org.bukkit.ChatColor;
+
+public class Utils {
+	public static String chat(String s) {
+		return ChatColor.translateAlternateColorCodes('&', s);
+	}
+}
